85cd8439ea343b600e37aaa5ed3e1617f2795952
Lemon <129915913+PinkLemon2004@users.noreply.github.com>
2023-04-05 09:12:02 +0700

    Add files via upload

---
+++ Volume_of_the _box.py
@@ -0,0 +1,7 @@
+width = (int(input("Enter width ==>")))
+length = (int(input("Enter length ==>")))
+height = (int(input("Enter height ==>")))
+
+box = width * length * height
+
+print("Volume of the box = ", box)
